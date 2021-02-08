f3ee31bf5f930fea13d03f8a2d2874a06711947a
shakibshalim2 <mdshakibshalim@gmail.com>
2021-02-08 21:05:12 +0600

    assignment done

--- script.js
+++ script.js
@@ -1,102 +1,75 @@
-
-const searchBtn = document.getElementById('search');
-const mealItem = document.getElementById('meal');
-const recipeDetails = document.querySelector('.recipe-details')
-
-
-searchBtn.addEventListener('click', foodList);
-mealItem.addEventListener('click', getRecipe);
-function foodList() {
-    let searchInput = document.getElementById('search-box').value;
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInput}`)
-        .then(res => res.json())
-        .then(data => {
-            let food = '';
-            if (data.meals) {
-                data.meals.forEach(meal => {
-                    food += `
-    <div class="meal-item shadow-none bg-light w-25 col-md-3" data-id= "${meal.idMeal}">
-    <div class="row food-size ">
-    <div class="meal-img">
-    <img class="img-fluid" src="${meal.strMealThumb}" alt="food">
- </div>
- <div class="food-name">
-    <h2><a href="" class="get-recipe fs-3">${meal.strMeal}</a></h2>
- </div>
- 
- 
-    </div>
-</div>
-    `;
- }); 
- }
-else {
-  food = "Sorry The Meal didn't have in our list"
-
- }
-   mealItem.innerHTML = food;
- });
-
-
-}
-function getRecipe(event) {
-    event.preventDefault();
-    if (event.target.classList.contains('get-recipe')) {
-        let mealItem = event.target.parentElement.parentElement;
-        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem}`)
-            .then(res => res.json())
-            .then(data => mealRecipeList(data.meals[0])
-
-            );
+const searchMeals = () =>{
+    const searchMeal = document.getElementById('search-field').value;
+    const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchMeal}`
+    //load data
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayFoods(data.meals))
+    .catch(error => console.log(error))
+    
     }
-
-}
-
-function mealRecipeList(meal) {
-    console.log(meal);
-    let recipe = `
-<h2 class="recipe-title">${meal.strMeal}</h2>
-                <p class="recipe-list">${meal.strCategory}</p>
-                <div class="instruction">
-                    <h2>instruction</h2>
-                    <p>${meal.strInstructions}</p>
-
-                </div>
-                <div class="recipe-img w-50 ms-5">
-                    <img class="img-fluid ms-5 ps-4 d-block" src="${meal.strMealThumb}" alt="">
-                </div>
-
-`;
-    recipeDetails.innerHTML = recipe;
-    recipeDetails.parentElement.classList.add('showRecipe')
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    
+    const displayFoods = foods =>{
+    const foodContainer = document.getElementById('food-container')
+    foodContainer.innerHTML = '';
+    
+    foods.forEach(meal =>{
+    const foodDiv = document.createElement('div')
+    foodDiv.className = 'single-result';
+    foodDiv.innerHTML = `
+    <div onclick="getFoodDetails('${meal.idMeal}')" class="col-md-12" data-id= "${meal.idMeal}">
+    <div class="meal-img">
+    <img class="img-fluid d-block" src="${meal.strMealThumb}" alt="food">
+    </div>
+    <div class="food-name pt-3 ps-2">
+    <h2>${meal.strMeal}</h2>
+    </div>
+    </div>
+    
+    
+    
+    `;
+    foodContainer.appendChild(foodDiv);
+    
+    });
+    
+    
+    
+    }
+    
+    function getFoodDetails (foodId){
+        
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodId}
+    `)
+    .then(res => res.json())
+    .then(data => displayFoodDetails(data.meals[0])
+    )
+    }
+    
+    function displayFoodDetails (foodRecipe) {
+    
+    const recipeDiv = document.getElementById('recipe')
+    const mealContainer = document.getElementById('food-container')
+    recipeDiv.innerHTML = `
+    <h2 class="recipe-title">food Details</h2>
+    <p class="recipe-list">${foodRecipe.strMeal}</p>
+    <div class="instruction align-items-center">
+        <h2>ingredients</h2>
+        <p>${foodRecipe.strIngredient1} ${foodRecipe.strMeasure1}</p>
+        <p>${foodRecipe.strIngredient2} ${foodRecipe.strMeasure2}</p>
+        <p>${foodRecipe.strIngredient3} ${foodRecipe.strMeasure3}</p>
+        <p>${foodRecipe.strIngredient4} ${foodRecipe.strMeasure4}</p>
+        <p>${foodRecipe.strIngredient5} ${foodRecipe.strMeasure5}</p>
+        <p>${foodRecipe.strIngredient6} ${foodRecipe.strMeasure6}</p>
+        <p>${foodRecipe.strIngredient7} ${foodRecipe.strMeasure7}</p>
+        <p>${foodRecipe.strIngredient8} ${foodRecipe.strMeasure8}</p>
+        <p>${foodRecipe.strIngredient9} ${foodRecipe.strMeasure9}</p>
+        
+    
+    </div>
+    <div class="recipe-img w-75">
+        <img class="img-fluid ms-5 ps-4 d-block img-size" src="${foodRecipe.strMealThumb}" alt="">
+       
+    </div>`;
+    }
